a453e06daa6337f95b06fe48dc28df01c88523d6
yosias hailu <yosiashailu@yosiass-MBP-3.lan>
2024-05-01 18:16:16 -0400

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://bidly-front-end.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  });
   await app.listen(3001);
 }
 bootstrap();
