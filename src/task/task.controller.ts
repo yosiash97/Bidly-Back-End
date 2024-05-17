d01a50fba143bff4ec99a42858d2c35ee5eb54e7
yosias hailu <yosiashailu@yosiass-MBP-3.lan>
2024-05-17 18:19:33 -0400

    testing delilah

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -17,7 +17,7 @@ export class TaskController {
 
   @Get('distance')
   async dummy(@Query('sliderValue') sliderValue: number) {
-    console.log("Ya dead")
+    console.log("Head to Delilah")
     let records = await this.bidService.findAll();
     const homeDistance = `POINT 37.3387 121.8853`;
     let homeLat = 37.3387
